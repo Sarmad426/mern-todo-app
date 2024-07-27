1d61ed443e39b77cade00c63ee6260fe5d9bde6b
Sarmad rafique <sarmadrafique040@gmail.com>
2024-07-27 22:55:33 +0500

    chore: Update color scheme and styles for light mode

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class", // or 'media' for OS-based preference
   theme: {
     extend: {},
   },
+  variants: {},
   plugins: [],
 };
