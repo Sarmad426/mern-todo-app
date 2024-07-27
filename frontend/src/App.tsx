d5aad2917dbfda6b6bfe0ad19bc256305882e83d
Sarmad rafique <sarmadrafique040@gmail.com>
2024-07-27 22:55:47 +0500

    feat: Add theme switcher component to App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import "react-toastify/dist/ReactToastify.css";
 import AddTodo from "./components/AddTodo";
 import TodoList from "./components/Todolist";
 import { ITodo } from "./types/types";
+import ThemeSwitcher from "./components/ThemeSwitcher";
 
 const App: React.FC = () => {
   const [todos, setTodos] = useState<ITodo[]>([]);
@@ -17,13 +18,12 @@ const App: React.FC = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <div className="w-full max-w-md p-8 bg-white rounded-lg shadow-md">
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <ThemeSwitcher />
+      <div className="w-full max-w-md p-8 bg-white dark:bg-gray-800 text-gray-800 dark:text-white rounded-lg shadow-md">
         <div className="text-center mb-4">
-          <h1 className="text-4xl font-bold my-2 text-center text-gray-800">
-            Todo List
-          </h1>
-          <p className="text-gray-500">Make yourself productive</p>
+          <h1 className="text-4xl font-bold my-2 text-center">Todo List</h1>
+          <p className="text-gray-400">Make yourself productive</p>
         </div>
         <TodoList />
         <AddTodo onAdd={handleAdd} />
